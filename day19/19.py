98c72238a9bcc9c07f79be721c7bad3a941b9c8b
thorstel <thorstel@pm.me>
2020-12-30 21:27:51 +0100

    Solution for day 19.

---
+++ day19/19.py
@@ -0,0 +1,85 @@
+from collections import *
+from math import *
+import itertools
+import re
+import sys
+
+def solve(data):
+    rules, molecule = data.split('\n\n')
+    replacements = []
+    gen1 = set()
+    for r in rules.splitlines():
+        src, dst = r.split(' => ')
+        for i in range(len(molecule)):
+            if src in molecule[i:]:
+                gen1.add(molecule[:i] + molecule[i:].replace(src, dst, 1))
+        replacements.append((src, dst))
+    print(len(gen1))
+
+    # Part 2, this should not have worked 😬
+    steps = 0
+    while molecule != 'e':
+        for src, dst in replacements:
+            if dst in molecule:
+                steps += 1
+                molecule = molecule.replace(dst, src, 1)
+    print(steps)
+
+########################################################################
+#                             SETUP STUFF                              #
+########################################################################
+
+actual_input = r"""
+Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg
+
+CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF
+""".strip()
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as f:
+        file_input = f.read().strip()
+    solve(file_input)
+else:
+    solve(actual_input)
